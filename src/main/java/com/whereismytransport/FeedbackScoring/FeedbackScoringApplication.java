3e93316bc0014d3b3934efc064f0f4c67cbeaedc
Malesela Sithole <malesela.ms@gmail.com>
2022-01-04 11:47:49 +0200

    Spring Boot project init commit

---
+++ src/main/java/com/whereismytransport/FeedbackScoring/FeedbackScoringApplication.java
@@ -0,0 +1,13 @@
+package com.whereismytransport.FeedbackScoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackScoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whereismytransport/FeedbackScoring/FeedbackScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whereismytransport.FeedbackScoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
